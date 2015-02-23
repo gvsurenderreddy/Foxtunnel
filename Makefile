fc872478b4156103d3ef394ad13b33af7ac6518b
Ørjan Malde <foxyred333@gmail.com>
2015-02-23 10:29:00 +0100

    Add rest of files
    Because why not. ;)

---
+++ Makefile
@@ -0,0 +1,109 @@
+# Makefile for proxytunnel
+#
+# Please uncomment the appropriate settings
+
+name = proxytunnel
+version = $(shell awk 'BEGIN { FS="\"" } /^\#define VERSION / { print $$2 }' config.h)
+
+ifneq ($(wildcard .svn),)
+revision = $(shell svnversion | awk 'BEGIN { RS=":" } { next } END { print $$1 }')
+else
+revision = $(shell echo '$$Revision$$' | sed -e 's/\$$Revision: \([0-9]\+\) \$$$$/\1/')
+endif
+
+CC = gcc
+CFLAGS = -Wall -O2 -ggdb
+
+OPTFLAGS = -DREVISION=\"$(revision)\"
+
+# Comment on non-gnu systems
+OPTFLAGS += -DHAVE_GETOPT_LONG
+
+# Comment if you don't have/want ssl
+OPTFLAGS += -DUSE_SSL
+
+# Uncomment to use TLSv1.1 else TLSv1 will be used
+#OPTFLAGS += -DUSE_TLS_v1_1
+
+# Most systems
+OPTFLAGS += -DSETPROCTITLE -DSPT_TYPE=2 #Broken on Cygwin?
+
+# Comment if you don't have this flag
+OPTFLAGS += -DSO_REUSEPORT
+
+# System dependant blocks... if your system is listed below, uncomment
+# the relevant lines
+
+# OpenBSD
+#OPTFLAGS += -DHAVE_SYS_PSTAT_H
+
+# DARWIN
+#OPTFLAGS += -DDARWIN
+
+# CYGWIN
+#OPTFLAGS += -DCYGWIN
+
+# SOLARIS
+#LDFLAGS += -lsocket -lnsl
+#LDFLAGS += -L/usr/local/ssl/lib	# Path to your SSL lib dir
+
+# END system dependant block
+
+SSL_LIBS := $(shell pkg-config --libs libssl 2>/dev/null)
+ifeq ($(SSL_LIBS),)
+SSL_LIBS := $(shell pkg-config --libs openssl 2>/dev/null)
+endif
+ifeq ($(SSL_LIBS),)
+SSL_LIBS := -lssl -lcrypto
+endif
+LDFLAGS += $(SSL_LIBS)
+
+prefix = /usr/local
+bindir = $(prefix)/bin
+datadir = $(prefix)/share
+mandir = $(datadir)/man
+
+# Remove strlcpy/strlcat on (open)bsd/darwin systems
+OBJ = proxytunnel.o	\
+	base64.o	\
+	strzcat.o	\
+	setproctitle.o	\
+	io.o		\
+	http.o		\
+	basicauth.o	\
+	readpassphrase.o	\
+	messages.o	\
+	cmdline.o	\
+	ntlm.o		\
+	ptstream.o
+
+.PHONY: all clean docs install
+
+all: proxytunnel # docs
+
+docs:
+	$(MAKE) -C docs
+
+proxytunnel: $(OBJ)
+	$(CC) -o $(name) $(CFLAGS) $(OPTFLAGS) $(OBJ) $(LDFLAGS)
+
+clean:
+	@rm -f $(name) $(OBJ)
+	$(MAKE) -C docs clean
+
+install:
+	install -Dp -m0755 $(name) $(DESTDIR)$(bindir)/$(name)
+	$(MAKE) -C docs install
+
+.c.o:
+	$(CC) $(CFLAGS) $(OPTFLAGS) -c -o $@ $<
+
+dist: clean docs
+	sed -i -e 's/^Version:.*$$/Version: $(version)/' contrib/proxytunnel.spec
+	find . ! -wholename '*/.svn*' | pax -d -w -x ustar -s ,^./,$(name)-$(version)/, | bzip2 >../$(name)-$(version).tar.bz2
+
+rpm: dist
+	rpmbuild -tb --clean --rmsource --rmspec --define "_rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm" --define "_rpmdir ../" ../$(name)-$(version).tar.bz2
+
+srpm: dist
+	rpmbuild -ts --clean --rmsource --rmspec --define "_rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm" --define "_srcrpmdir ../" ../$(name)-$(version).tar.bz2
---
+++ ptstream.c
@@ -0,0 +1,201 @@
+/* Proxytunnel - (C) 2001-2008 Jos Visser / Mark Janssen    */
+/* Contact:                  josv@osp.nl / maniac@maniac.nl */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* ptstream.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "proxytunnel.h"
+
+
+/* Open a stream for incoming and outgoing data with the specified fds */
+PTSTREAM *stream_open(int incoming_fd, int outgoing_fd) {
+	PTSTREAM *pts;
+
+	/* Initialise the structure and store the file descriptor */
+	pts = malloc(sizeof(PTSTREAM));
+	pts->incoming_fd = incoming_fd;
+	pts->outgoing_fd = outgoing_fd;
+	pts->ssl = NULL;
+	pts->ctx = NULL;
+
+	/* Return a pointer to the structure */
+	return pts;
+}
+
+
+/* Close a stream */
+int stream_close(PTSTREAM *pts) {
+#ifdef USE_SSL
+	/* Destroy the SSL context */
+	if (pts->ssl) {
+		SSL_shutdown (pts->ssl);
+		SSL_free (pts->ssl);
+		SSL_CTX_free (pts->ctx);
+	}
+#endif /* USE_SSL */
+
+	/* Close the incoming fd */
+	close(pts->incoming_fd);
+
+	/* Close the outgoing fd */
+	close(pts->outgoing_fd);
+
+	/* Free the structure */
+	free(pts);
+
+	return 1;
+}
+
+
+/* Read from a stream */
+int stream_read(PTSTREAM *pts, void *buf, size_t len) {
+	/* Read up to the specified number of bytes into the buffer */
+	int bytes_read;	
+
+	if (!pts->ssl) {
+		/* For a non-SSL stream... */
+		bytes_read = read(pts->incoming_fd, buf, len);
+	} else {
+#ifdef USE_SSL
+		/* For an SSL stream... */
+		bytes_read = SSL_read(pts->ssl, buf, len);
+#else
+		/* No SSL support, so must use a non-SSL stream */
+		bytes_read = read(pts->incoming_fd, buf, len);
+#endif /* USE_SSL */
+	}
+
+	return bytes_read;
+}
+
+
+/* Write to a stream */
+int stream_write(PTSTREAM *pts, void *buf, size_t len) {
+	/* Write the specified number of bytes from the buffer */
+	int bytes_written;
+
+	if (!pts->ssl) {
+		/* For a non-SSL stream... */
+		bytes_written = write(pts->outgoing_fd, buf, len);
+	} else {
+#ifdef USE_SSL
+		/* For an SSL stream... */
+		bytes_written = SSL_write(pts->ssl, buf, len);
+#else
+		/* No SSL support, so must use a non-SSL stream */
+		bytes_written = write(pts->outgoing_fd, buf, len);
+#endif /* USE_SSL */
+	}
+
+	return bytes_written;
+}
+
+
+/*
+ * Copy a block of data from one stream to another. A true
+ * return code signifies EOF on the from socket descriptor.
+ */
+int stream_copy(PTSTREAM *pts_from, PTSTREAM *pts_to) {
+	char buf[SIZE];
+	int n;
+
+	/* Read a buffer from the source socket */
+	if ( ( n = stream_read( pts_from, buf, SIZE ) ) < 0 ) {
+		my_perror( "Socket read error" );
+		exit( 1 );
+	}
+
+	/* If we have read 0 bytes, there is an EOF on src */
+	if( n==0 )
+		return 1;
+
+	/* Write the buffer to the destination socket */
+	if ( stream_write( pts_to, buf, n ) != n ) {
+		my_perror( "Socket write error" );
+		exit( 1 );
+	}
+
+	/* We're not yet at EOF */
+	return 0;
+}
+
+
+/* Initiate an SSL handshake on this stream and encrypt all subsequent data */
+int stream_enable_ssl(PTSTREAM *pts) {
+#ifdef USE_SSL
+	SSL_METHOD *meth;
+	SSL *ssl;
+	SSL_CTX *ctx;
+	
+	/* Initialise the connection */
+	SSLeay_add_ssl_algorithms();
+	#ifdef USE_TLS_v1_1
+	meth = TLSv1_1_client_method();
+	#else
+	meth = TLSv1_client_method();
+	#endif
+	SSL_load_error_strings();
+
+	ctx = SSL_CTX_new (meth);
+	ssl = SSL_new (ctx);
+	SSL_set_rfd (ssl, stream_get_incoming_fd(pts));
+	SSL_set_wfd (ssl, stream_get_outgoing_fd(pts));	
+	SSL_connect (ssl);
+
+	/* Store ssl and ctx parameters */
+	pts->ssl = ssl;
+	pts->ctx = ctx;
+#else
+	message("Warning: stream_open(): SSL stream requested but no SSL support available; using unencrypted connection");
+#endif /* USE_SSL */
+
+	return 1;
+}
+
+
+/* Return the incoming_fd for a given stream */
+int stream_get_incoming_fd(PTSTREAM *pts) {
+
+	if (!pts->ssl)
+		return pts->incoming_fd;
+	else
+#ifdef USE_SSL
+		return SSL_get_rfd(pts->ssl);
+#else
+		return pts->incoming_fd;
+#endif /* USE_SSL */
+}
+
+/* Return the outgoing_fd for a given stream */
+int stream_get_outgoing_fd(PTSTREAM *pts) {
+	if (!pts->ssl)
+		return pts->outgoing_fd;
+	else
+#ifdef USE_SSL
+		return SSL_get_wfd(pts->ssl);
+#else
+		return pts->outgoing_fd;
+#endif /* USE_SSL */
+}
